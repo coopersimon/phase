c23ff6ff6600789a94ccd46e436e8feae1de72da
Simon Cooper <simon.d.cooper@hotmail.co.uk>
2026-01-27 14:44:48 +0100

    Separate renderer thread, moved status to render thread

--- phase/src/gpu/mod.rs
+++ phase/src/gpu/mod.rs
@@ -1,50 +1,63 @@
 mod videostate;
+mod renderer;
 
-use std::collections::VecDeque;
+use std::sync::{
+    Arc,
+    atomic::{AtomicU32, Ordering}
+};
 
 use mips::mem::Data;
 
+use crossbeam_channel::{
+    bounded, Sender, Receiver
+};
+
 use crate::{
-    mem::{DMADevice, ram::RAM},
+    mem::DMADevice,
     utils::{bits::*, interface::MemInterface}
 };
 
+use renderer::*;
 use videostate::StateMachine;
 pub use videostate::GPUClockRes;
 
-const VRAM_SIZE: usize = 1024 * 1024;
-
 /// Graphics processing unit
 pub struct GPU {
-    vram: RAM,
-    status: GPUStatus,
-
     state: StateMachine,
 
-    gp0_fifo: VecDeque<u32>,
+    status: Arc<AtomicU32>,
     read_reg: u32,
+
+    renderer_tx: Sender<RendererCmd>,
 }
 
 impl GPU {
     pub fn new() -> Self {
+        let (tx, rx) = bounded(32); // TODO: should this even be bounded..?
+        let status = Arc::new(AtomicU32::new(0));
+        let thread_status = status.clone();
+        // Start render thread.
+        std::thread::spawn(|| {
+            let mut renderer = Renderer::new(rx, thread_status);
+            renderer.run();
+        });
         Self {
-            vram: RAM::new(VRAM_SIZE),
-            status: GPUStatus::CommandReady | GPUStatus::DMARecvReady,
-
             state: StateMachine::new(),
 
-            gp0_fifo: VecDeque::new(),
+            status: status,
             read_reg: 0,
+
+            renderer_tx: tx,
         }
     }
 
     pub fn clock(&mut self, cycles: usize) -> GPUClockRes {
         let res = self.state.clock(cycles);
 
-        while let Some(data) = self.gp0_fifo.pop_front() {
+        /*while let Some(data) = self.gp0_fifo.pop_front() {
             self.exec_gp0_command(data);
         }
-        self.status.insert(GPUStatus::CommandReady | GPUStatus::DMARecvReady);
+        self.status.insert(GPUStatus::CommandReady | GPUStatus::DMARecvReady);*/
 
         res
     }
@@ -52,8 +65,8 @@ impl GPU {
     /// Check if the GPU is ready to transfer via DMA,
     /// using a command list.
     pub fn dma_command_ready(&self) -> bool {
-        let mask = GPUStatus::DMAMode | GPUStatus::DMARecvReady;
-        (self.status & mask).bits() == GPUStatus::CommandTransferReady.bits()
+        let mask = (GPUStatus::DMAMode | GPUStatus::DMARecvReady).bits();
+        (self.status.load(Ordering::Acquire) & mask) == GPUStatus::CommandTransferReady.bits()
     }
 }
 
@@ -82,7 +95,7 @@ impl DMADevice for GPU {
     }
 
     fn dma_write_word(&mut self, data: u32) -> usize {
-        let dma_mode = (self.status & GPUStatus::DMAMode).bits() >> 29;
+        let dma_mode = (self.status.load(Ordering::Acquire) & GPUStatus::DMAMode.bits()) >> 29;
         if dma_mode == 2 {
             self.send_gp0_command(data);
         } else {
@@ -95,7 +108,7 @@ impl DMADevice for GPU {
 // Internal
 impl GPU {
     fn send_gp0_command(&mut self, data: u32) {
-        self.gp0_fifo.push_back(data);
+        let _ = self.renderer_tx.send(RendererCmd::GP0(data));
     }
 
     fn send_gp1_command(&mut self, data: u32) {
@@ -122,37 +135,16 @@ impl GPU {
     }
 
     fn read_status(&self) -> u32 {
-        if self.status.contains(GPUStatus::Interlace) {
-            let mut stat = self.status;
-            stat.set(GPUStatus::InterlaceOdd, self.state.get_interlace_bit());
-            stat.bits()
+        let status = self.status.load(Ordering::Acquire);
+        if status & GPUStatus::Interlace.bits() != 0 {
+            if self.state.get_interlace_bit() {
+                status | GPUStatus::InterlaceOdd.bits()
+            } else {
+                status
+            }
         } else {
-            self.status.bits()
+            status
         }
-    }
-
-    fn exec_gp0_command(&mut self, data: u32) {
-        println!("GP0 command: {:X}", data);
-        let command = (data >> 24) as u8;
-        match command {
-            0xE1 => self.draw_mode_setting(data),
-            _ => {}, // Invalid command.
-        }
-    }
-}
-
-// GP0 commands
-impl GPU {
-
-    // Rendering attribute commands
-
-    fn draw_mode_setting(&mut self, param: u32) {
-        let low_bits = param & 0x7FF;
-        self.status.remove(GPUStatus::DrawModeFlags);
-        self.status.insert(GPUStatus::from_bits_truncate(low_bits));
-        self.status.set(GPUStatus::TexDisable, test_bit!(param, 11));
-
-        // TODO: x-flip and y-flip
     }
 }
 
@@ -172,26 +164,25 @@ impl GPU {
     }
 
     fn reset_command_buf(&mut self) {
-
+        // TODO: flush tx.
     }
 
     fn acknowledge_irq(&mut self) {
-        self.status.remove(GPUStatus::IRQ);
+        let _ = self.renderer_tx.send(RendererCmd::AcknowledgeIRQ);
     }
 
     fn display_enable(&mut self, param: u32) {
-        self.status.set(GPUStatus::DisplayEnable, !test_bit!(param, 0));
+        let enable = !test_bit!(param, 0);
+        let _ = self.renderer_tx.send(RendererCmd::DisplayEnable(enable));
     }
 
     fn data_request(&mut self, param: u32) {
         let dma_mode = param & 0x3;
-        self.status.remove(GPUStatus::DMAMode);
-        self.status.insert(GPUStatus::from_bits_truncate(dma_mode << 29));
+        let _ = self.renderer_tx.send(RendererCmd::DataRequest(GPUStatus::from_bits_truncate(dma_mode << 29)));
     }
 
     fn display_vram_offset(&mut self, param: u32) {
-        let _x = param & 0x3FF;
-        let _y = (param >> 10) & 0x1FF;
+        let _ = self.renderer_tx.send(RendererCmd::DisplayVRAMOffset(param));
     }
 
     fn display_range_x(&mut self, param: u32) {
@@ -207,23 +198,24 @@ impl GPU {
     }
 
     fn display_mode(&mut self, param: u32) {
-        self.status.remove(GPUStatus::DispModeFlags);
-        self.status.insert(GPUStatus::from_bits_truncate((param & 0x3F) << 17));
-        self.status.insert(GPUStatus::from_bits_truncate((param & 0x40) << 10)); // HRes low bit
-        self.status.insert(GPUStatus::from_bits_truncate((param & 0x80) << 7)); // Reverseflag
-        let h_res = match (self.status & GPUStatus::XResolution).bits() >> 16 {
+        let mut display_status = GPUStatus::empty();
+        display_status.insert(GPUStatus::from_bits_truncate((param & 0x3F) << 17));
+        display_status.insert(GPUStatus::from_bits_truncate((param & 0x40) << 10)); // HRes low bit
+        display_status.insert(GPUStatus::from_bits_truncate((param & 0x80) << 7)); // Reverseflag
+        let h_res = match (display_status & GPUStatus::XResolution).bits() >> 16 {
             0b000 => 256,
             0b010 => 320,
             0b100 => 512,
             0b110 => 640,
             _ => 368,
         };
-        if self.status.contains(GPUStatus::PALMode) {
+        if display_status.contains(GPUStatus::PALMode) {
             panic!("PAL mode unsupported!");
         } else {
             self.state.set_h_res_ntsc(h_res);
         }
-        self.state.set_interlace(self.status.contains(GPUStatus::Interlace));
+        self.state.set_interlace(display_status.contains(GPUStatus::Interlace));
+        let _ = self.renderer_tx.send(RendererCmd::DisplayMode(display_status));
     }
 
     fn get_gpu_info(&mut self, param: u32) {
@@ -237,41 +229,7 @@ impl GPU {
     }
 
     fn tex_disable(&mut self, param: u32) {
-        self.status.set(GPUStatus::TexDisable, test_bit!(param, 0));
-    }
-}
-
-bitflags::bitflags! {
-    #[derive(Clone, Copy)]
-    struct GPUStatus: u32 {
-        const InterlaceOdd  = bit!(31);
-        const DMAMode       = bits![29, 30];
-        const DMARecvReady  = bit!(28);
-        const VRAMSendReady = bit!(27);
-        const CommandReady  = bit!(26);
-        const DMARequest    = bit!(25);
-        const IRQ           = bit!(24);
-        const DisplayEnable = bit!(23);
-        const Interlace     = bit!(22);
-        const ColorDepth    = bit!(21);
-        const PALMode       = bit!(20);
-        const YResolution   = bit!(19);
-        const XResolution   = bits![16, 17, 18];
-        const TexDisable    = bit!(15);
-        const Reverse       = bit!(14);
-        const InterlaceField = bit!(13);
-        const MaskDrawing   = bit!(12);
-        const SetDrawMask   = bit!(11);
-        const DrawDisplay   = bit!(10);
-        const Dither        = bit!(9);
-        const TexPageCol    = bits![7, 8];
-        const SemiTrans     = bits![5, 6];
-        const TexPageYBase  = bit!(4);
-        const TexPageXBase  = bits![0, 1, 2, 3];
-
-        const DispModeFlags = bits![14, 16, 17, 18, 19, 20, 21, 22];
-        const DrawModeFlags = bits![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15];
-
-        const CommandTransferReady = bits![28, 30];
+        let disable = test_bit!(param, 0);
+        let _ = self.renderer_tx.send(RendererCmd::TexDisable(disable));
     }
 }
---
+++ phase/src/gpu/renderer.rs
@@ -0,0 +1,183 @@
+
+use std::sync::{
+    Arc,
+    atomic::{Ordering, AtomicU32}
+};
+
+use crossbeam_channel::{
+    Sender, Receiver
+};
+
+use crate::{
+    mem::ram::RAM,
+    utils::bits::*
+};
+
+const VRAM_SIZE: usize = 1024 * 1024;
+
+pub enum RendererCmd {
+    /// A command word sent via GP0.
+    /// This could be a full or partial command,
+    /// or data to be written to VRAM.
+    GP0(u32),
+    /// A new frame has begun, and we want to capture
+    /// the frame buffer.
+    GetFrame,
+
+    // GP1 commands:
+    AcknowledgeIRQ,
+    DisplayEnable(bool),
+    DataRequest(GPUStatus),
+    DisplayVRAMOffset(u32),
+    DisplayMode(GPUStatus),
+    TexDisable(bool),
+}
+
+pub enum GP1Command {
+    DisplayEnable,
+
+}
+
+/// GPU renderer.
+/// 
+/// This lives on a different thread.
+/// It receives GP0 commands and dispatches render calls.
+/// 
+/// It also manages VRAM.
+pub struct Renderer {
+    // Comms
+    command_rx: Receiver<RendererCmd>,
+    atomic_status: Arc<AtomicU32>,
+
+    // Internal state
+    vram: RAM,
+    status: GPUStatus,
+}
+
+impl Renderer {
+    pub fn new(command_rx: Receiver<RendererCmd>, status: Arc<AtomicU32>) -> Self {
+        let init_status = GPUStatus::CommandReady | GPUStatus::DMARecvReady;
+        status.store(init_status.bits(), Ordering::Release);
+        Self {
+            command_rx,
+            atomic_status: status,
+
+            vram: RAM::new(VRAM_SIZE),
+            status: init_status,
+        }
+    }
+
+    /// Run in a separate thread.
+    pub fn run(&mut self) {
+        use RendererCmd::*;
+        while let Ok(cmd) = self.command_rx.recv() {
+            match cmd {
+                GP0(data)                   => self.exec_gp0_command(data),
+                GetFrame                    => self.send_frame(),
+                AcknowledgeIRQ              => self.acknowledge_irq(),
+                DisplayEnable(enable)       => self.display_enable(enable),
+                DataRequest(data_req_stat)  => self.data_request(data_req_stat),
+                DisplayVRAMOffset(offset)   => self.display_vram_offset(offset),
+                DisplayMode(disp_mode_stat) => self.display_mode(disp_mode_stat),
+                TexDisable(disable)         => self.tex_disable(disable),
+            }
+        }
+    }
+
+    fn send_frame(&mut self) {
+
+    }
+}
+
+// GP1.
+impl Renderer {
+    fn acknowledge_irq(&mut self) {
+        self.status.remove(GPUStatus::IRQ);
+        self.atomic_status.store(self.status.bits(), Ordering::Release);
+    }
+
+    fn display_enable(&mut self, enable: bool) {
+        self.status.set(GPUStatus::DisplayEnable, enable);
+        self.atomic_status.store(self.status.bits(), Ordering::Release);
+    }
+
+    fn data_request(&mut self, data_req_stat: GPUStatus) {
+        self.status.remove(GPUStatus::DMAMode);
+        self.status.insert(data_req_stat);
+        self.atomic_status.store(self.status.bits(), Ordering::Release);
+    }
+
+    fn display_vram_offset(&mut self, offset: u32) {
+        let _x = offset & 0x3FF;
+        let _y = (offset >> 10) & 0x1FF;
+    }
+
+    fn display_mode(&mut self, disp_mode_stat: GPUStatus) {
+        self.status.remove(GPUStatus::DispModeFlags);
+        self.status.insert(disp_mode_stat);
+        self.atomic_status.store(self.status.bits(), Ordering::Release);
+    }
+
+    fn tex_disable(&mut self, disable: bool) {
+        self.status.set(GPUStatus::TexDisable, disable);
+        self.atomic_status.store(self.status.bits(), Ordering::Release);
+    }
+}
+
+// GP0.
+impl Renderer {
+    fn exec_gp0_command(&mut self, data: u32) {
+        println!("GP0 command: {:X}", data);
+        let command = (data >> 24) as u8;
+        match command {
+            
+            0xE1 => self.draw_mode_setting(data),
+            _ => {}, // Invalid command.
+        }
+    }
+
+    fn draw_mode_setting(&mut self, param: u32) {
+        let low_bits = param & 0x7FF;
+        self.status.remove(GPUStatus::DrawModeFlags);
+        self.status.insert(GPUStatus::from_bits_truncate(low_bits));
+        self.status.set(GPUStatus::TexDisable, test_bit!(param, 11));
+        self.atomic_status.store(self.status.bits(), Ordering::Release);
+
+        // TODO: x-flip and y-flip
+    }
+}
+
+bitflags::bitflags! {
+    #[derive(Clone, Copy)]
+    pub struct GPUStatus: u32 {
+        const InterlaceOdd  = bit!(31);
+        const DMAMode       = bits![29, 30];
+        const DMARecvReady  = bit!(28);
+        const VRAMSendReady = bit!(27);
+        const CommandReady  = bit!(26);
+        const DMARequest    = bit!(25);
+        const IRQ           = bit!(24);
+        const DisplayEnable = bit!(23);
+        const Interlace     = bit!(22);
+        const ColorDepth    = bit!(21);
+        const PALMode       = bit!(20);
+        const YResolution   = bit!(19);
+        const XResolution   = bits![16, 17, 18];
+        const TexDisable    = bit!(15);
+        const Reverse       = bit!(14);
+        const InterlaceField = bit!(13);
+        const MaskDrawing   = bit!(12);
+        const SetDrawMask   = bit!(11);
+        const DrawDisplay   = bit!(10);
+        const Dither        = bit!(9);
+        const TexPageCol    = bits![7, 8];
+        const SemiTrans     = bits![5, 6];
+        const TexPageYBase  = bit!(4);
+        const TexPageXBase  = bits![0, 1, 2, 3];
+
+        const DispModeFlags = bits![14, 16, 17, 18, 19, 20, 21, 22];
+        const DrawModeFlags = bits![0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15];
+
+        const CommandTransferReady = bits![28, 30];
+    }
+}
