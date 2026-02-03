8163cac75bc7c9efcd94b5991f9f393025df3f31
Simon Cooper <simon.d.cooper@hotmail.co.uk>
2026-02-03 16:41:36 +0100

    IO comms simplified

--- phase/src/io.rs
+++ phase/src/io.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 use super::{Frame, Port};
 
 use crossbeam_channel::{
-    Sender, Receiver, unbounded
+    Sender, Receiver, bounded
 };
 use std::sync::{
     Arc, Mutex
@@ -17,7 +17,6 @@ pub type Input = Box<[InputMessage]>;
 /// 
 /// TODO: AUDIO.
 pub struct IO {
-    frame_rx: Receiver<()>,
     input_tx: Sender<Input>,
     
     /// We hold a single frame internally, and then copy over
@@ -27,17 +26,14 @@ pub struct IO {
 
 impl IO {
     pub fn new() -> (Self, BusIO) {
-        let (frame_tx, frame_rx) = unbounded();
-        let (input_tx, input_rx) = unbounded();
+        let (input_tx, input_rx) = bounded(0);
         let frame = Arc::new(Mutex::new(Frame::new()));
         let io = Self {
-            frame_rx,
             input_tx,
 
             frame: frame.clone(),
         };
         let bus_io = BusIO {
-            frame_tx,
             input_rx,
 
             frame,
@@ -48,12 +44,10 @@ impl IO {
     /// Blocks until a frame is ready from the system.
     pub fn get_frame(&mut self, input: Input, frame: &mut Frame) {
         if self.input_tx.send(input).is_ok() {
-            if self.frame_rx.recv().is_ok() {
-                let frame_data = self.frame.lock().unwrap();
-                frame.size = frame_data.size;
-                frame.frame_buffer.resize(frame_data.frame_buffer.len(), 0);
-                frame.frame_buffer.copy_from_slice(&frame_data.frame_buffer);
-            }
+            let frame_data = self.frame.lock().unwrap();
+            frame.size = frame_data.size;
+            frame.frame_buffer.resize(frame_data.frame_buffer.len(), 0);
+            frame.frame_buffer.copy_from_slice(&frame_data.frame_buffer);
         }
     } 
 }
@@ -61,7 +55,6 @@ impl IO {
 /// The component of the I/O system that lives
 /// on the CPU/Memory Bus side.
 pub struct BusIO {
-    frame_tx: Sender<()>,
     input_rx: Receiver<Input>,
 
     /// The frame isn't actually used here, it's just passed
@@ -72,7 +65,6 @@ pub struct BusIO {
 impl BusIO {
     pub fn send_frame(&mut self) -> Input {
         if let Some(i) = self.input_rx.recv().ok() {
-            let _ = self.frame_tx.send(());
             i
         } else {
             // The channel has been dropped. This either means
